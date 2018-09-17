deac38b8f8c2e28dab3b50e6e13a2d15e8751556
kamatte <uydasan@gmail.com>
2018-09-17 21:10:29 +0900

    refactored

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const app = require('express')()
 const { Nuxt, Builder } = require('nuxt')
 
-const isProd = (process.env.NODE_ENV === 'production')
-const PORT = isProd ? 8080 : 3000
+const PORT = process.env.PORT || 3000
 
 // We instantiate nuxt.js with the options
 const config = require('./nuxt.config.js')
-config.dev = !isProd
+config.dev = process.env.NODE_ENV !== 'production'
 const nuxt = new Nuxt(config)
 
 // Render every route with Nuxt.js
